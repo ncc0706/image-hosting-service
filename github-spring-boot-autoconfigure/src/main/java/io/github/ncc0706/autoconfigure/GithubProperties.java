2cb6af1459fda2f46429c9f0179a4595816ae6bf
niuyuxian <ncc0706@gmail.com>
2020-06-04 10:41:15 +0800

    新增上传接口

--- github-spring-boot-autoconfigure/src/main/java/io/github/ncc0706/autoconfigure/GithubProperties.java
+++ github-spring-boot-autoconfigure/src/main/java/io/github/ncc0706/autoconfigure/GithubProperties.java
@@ -5,7 +5,39 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "github")
 public class GithubProperties {
 
+    private String api = "https://api.github.com/repos/";
+
+    private String jsdelivr = "https://cdn.jsdelivr.net/gh/";
+
+    // Github 用户名
     private String username;
+
+    // 仓库名称
+    private String repos;
+
+    private String name;
+
+    private String email;
+
+    private String message = "first commit";
+
+    private String token;
+
+    public String getApi() {
+        return api;
+    }
+
+    public void setApi(String api) {
+        this.api = api;
+    }
+
+    public String getJsdelivr() {
+        return jsdelivr;
+    }
+
+    public void setJsdelivr(String jsdelivr) {
+        this.jsdelivr = jsdelivr;
+    }
 
     public String getUsername() {
         return username;
@@ -13,5 +45,45 @@ public class GithubProperties {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getRepos() {
+        return repos;
+    }
+
+    public void setRepos(String repos) {
+        this.repos = repos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
--- github-spring-boot-autoconfigure/src/main/java/io/github/ncc0706/autoconfigure/service/GithubService.java
+++ github-spring-boot-autoconfigure/src/main/java/io/github/ncc0706/autoconfigure/service/GithubService.java
@@ -1,13 +1,72 @@
 package io.github.ncc0706.autoconfigure.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.ncc0706.autoconfigure.GithubProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class GithubService {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public String hello() {
-        return "hello github";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private GithubProperties githubProperties;
+
+    public Map<String, String> upload(MultipartFile file) throws IOException {
+
+        String encode = Base64Utils.encodeToString(file.getBytes());
+
+        String originalFilename = file.getOriginalFilename();
+        String ext = originalFilename.substring(originalFilename.lastIndexOf("."), originalFilename.length());
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+
+        String fileName = uuid + ext;
+
+        String url = githubProperties.getApi() + githubProperties.getUsername() + "/" + githubProperties.getRepos() + "/contents/" + fileName;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Authorization", "token " + githubProperties.getToken());
+        Map<String, Object> params = new HashMap<>();
+        params.put("message", githubProperties.getMessage());
+        Map<String, String> committer = new HashMap<>();
+        committer.put("name", githubProperties.getName());
+        committer.put("email", githubProperties.getEmail());
+        params.put("committer", committer);
+        params.put("content", encode);
+        String data = new ObjectMapper().writeValueAsString(params);
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(data, headers);
+        ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.PUT, httpEntity, String.class);
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("response: {}", exchange.getBody());
+        }
+
+        String result = githubProperties.getJsdelivr() + githubProperties.getUsername() + "/" + githubProperties.getRepos() + "@latest/" + fileName;
+
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("url", result);
+        resultMap.put("UBB", "[img]" + result + "[/img]");
+        resultMap.put("markdown", "![](" + result + ")");
+        return resultMap;
     }
-
 }
